f6f8e962f696ff7e57e81ae746449eb79bd279b8
Batuhan Gökalp <bbatuhangokalp@gmail.com>
2025-03-22 02:06:14 +0300

    created login and register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Bill from "./pages/Bill";
+import Register from "./pages/Auth/Register";
+
 function App() {
+  const storedAuth = JSON.parse(localStorage.getItem("storedUser"));
+
   return (
-    <div className="w-full bg-blue-600">
-      fdsfdsfdsf
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {storedAuth ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/bills" element={<Bill />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </>
+        )}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
